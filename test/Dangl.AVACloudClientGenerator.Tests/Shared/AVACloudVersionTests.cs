0474187e1fc24ee8cab146b413c209ccdb3126dd
Georg Dangl <georg@dangl.me>
2018-08-07 17:41:37 +0200

    Add tests

---
+++ test/Dangl.AVACloudClientGenerator.Tests/Shared/AVACloudVersionTests.cs
@@ -0,0 +1,16 @@
+﻿using Dangl.AVACloudClientGenerator.Shared;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Dangl.AVACloudClientGenerator.Tests.Shared
+{
+    public class AVACloudVersionTests
+    {
+        [Fact]
+        public async Task CanGetVersion()
+        {
+            var version = await new AVACloudVersion().GetAvaCloudVersionAsync();
+            Assert.False(string.IsNullOrWhiteSpace(version));
+        }
+    }
+}
