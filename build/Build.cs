1f90330ee095e47fa9c7f8d4345f9adb034ec601
Georg Dangl <georg@dangl.me>
2024-07-25 14:44:19 +0200

    Add build target to publish Dart package

--- build/Build.cs
+++ build/Build.cs
@@ -13,6 +13,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Web;
 using static Nuke.Common.IO.FileSystemTasks;
@@ -212,6 +213,47 @@ namespace Dangl.AVACloudClientGenerator
 
         System.IO.Compression.ZipFile.CreateFromDirectory(outputPath, zipOutputPath);
     }
+
+    Target GenerateAndPublishDartClient => _ => _
+        .DependsOn(Compile)
+        .Executes(() =>
+        {
+            GenerateClient("Dart");
+
+            var clientRoot = OutputDirectory / "Dart";
+
+
+            if (!string.IsNullOrWhiteSpace(DartPublishVersionOverride))
+            {
+                var pubspecFile = clientRoot / "dart-client" / "pubspec.yaml";
+                var content = pubspecFile.ReadAllText();
+                var updatedContent = Regex.Split(content, @"\r\n?|\n")
+                    .Select(c => c.Trim().StartsWith("version:") ? $"version: {DartPublishVersionOverride}" : c)
+                    .Aggregate((c, n) => c + Environment.NewLine + n);
+                pubspecFile.WriteAllText(updatedContent);
+            }
+
+            (clientRoot / "dart-client" / "CHANGELOG.md").WriteAllText("# Dangl AVACloud Client"
+                + Environment.NewLine
+                + (DartPublishVersionOverride ?? GitVersion.NuGetVersion));
+            (clientRoot / "LICENSE.md").Move(clientRoot / "dart-client" / "LICENSE");
+
+            var tempFolder = RootDirectory / "temp-publish";
+            tempFolder.CreateOrCleanDirectory();
+            tempFolder.DeleteDirectory();
+            Task.Delay(100).Wait(); // There are sometimes timing issues with folder access
+            try
+            {
+                (clientRoot / "dart-client").Move(tempFolder);
+
+                StartProcess("dart", "pub publish", workingDirectory: tempFolder)
+                    .AssertZeroExitCode();
+            }
+            finally
+            {
+                tempFolder.DeleteDirectory();
+            }
+        });
 
     Target GenerateAndPublishTypeScriptNpmClient => _ => _
         .DependsOn(Compile)
